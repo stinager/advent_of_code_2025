1ab90980fb9967d16eac3078646ddebe994f3b4c
Stina Gerard <stina.gerard@thalesaleniaspace.com>
2025-12-12 15:36:50 +0100

    part1 day01.py

--- day01/day01.py
+++ day01/day01.py
@@ -1,14 +1,40 @@
-def get_password():
-  password = 50
-  with open('input-day01.txt') as file:
-      for line in file:
-        if line.startswith('L')
-          password = passw
-          the rest of the number will be substracted to start_point
-        else:
-          the rest of the number will be added to start_point
+import re
+
+def get_number_with_modulo(line):
+    if line[0] == 'R':
+        num = int(line[1:])
+    elif line[0] == 'L':
+        num = -int(line[1:])
+    else:
+        raise ValueError(f"Line must start with 'R' or 'L', got: {line}")
+    if abs(num) > 100:
+        return (1 if num > 0 else -1) * (abs(num) % 100)
+    return num
+
+def get_password(file):
+  code = 50
+  password = 0
+  number = 0
+  with open(file) as file:
+    for line in file:
+      number = get_number_with_modulo(line)
+      code = code + number
+      print(code)
+      if code < 0:
+        code = 100 + code
+      if code >= 100:
+        code = code - 100
+      if code == 0:
+        password += 1
+    if code > 99:
+      code = code - 99
+    if code < 0:
+      code = 99 - code
+  return password
 
 
 if __name__ == "__main__":
+    print("---Pre Test---")
+    print("Password: ", get_password('test.txt'))
     print("---Part One---")
-    print("Password: ", get_password())
+    print("Password: ", get_password('input-day01.txt'))
